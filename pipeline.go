7fe67531dbfdbb712edb9963cddcddc0d69b524f
ktr0731 <ktr@syfm.me>
2018-10-13 18:19:31 +0900

    add OutputWithContext

--- pipeline.go
+++ pipeline.go
@@ -2,16 +2,17 @@ package pipeline
 
 import (
 	"bytes"
+	"context"
 	"os"
 	"os/exec"
 )
 
-func Output(commands ...[]string) ([]byte, error) {
+func OutputWithContext(ctx context.Context, commands ...[]string) ([]byte, error) {
 	cmds := make([]*exec.Cmd, len(commands))
 	var err error
 
 	for i, c := range commands {
-		cmds[i] = exec.Command(c[0], c[1:]...)
+		cmds[i] = exec.CommandContext(ctx, c[0], c[1:]...)
 		if i > 0 {
 			if cmds[i].Stdin, err = cmds[i-1].StdoutPipe(); err != nil {
 				return nil, err
@@ -32,4 +33,8 @@ func Output(commands ...[]string) ([]byte, error) {
 		}
 	}
 	return out.Bytes(), nil
+}
+
+func Output(commands ...[]string) ([]byte, error) {
+	return OutputWithContext(context.Background(), commands...)
 }
